8c6125a59c225ca1fdb03759b0b9885f849fc2bb
arthur_blake <arthur_blake@localhost>
2007-07-24 22:01:49 +0000

    windows script to build both the jdbc3 and jdbc4 versions of log4jdbc

---
+++ scripts/build.cmd
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+rem this is a bit convoluted because two jdk's must be invoked...
+
+set ANT_HOME=c:\apache-ant-1.6.5
+
+set JAVA_HOME=C:\j2sdk1.4.2_13
+set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
+call java -version
+
+call ant -Djdbc.level=3 all
+
+set JAVA_HOME=c:\jdk1.6.0
+set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
+call java -version
+
+call ant -Djdbc.level=4 all
