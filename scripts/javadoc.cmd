425148e1184bf0663a0fd3bc5a99c8e542f072ea
arthur_blake <arthur_blake@localhost>
2007-07-25 23:27:42 +0000

    change scripts to support dual javadoc generation for JDBC 3/jdk 1.4 and JDBC 4/jdk 1.6 versions of log4jdbc

---
+++ scripts/javadoc.cmd
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+rem run ydoc for jdbc 3 with jdk 1.4 and
+rem run ydoc for jdbc 4 with jdk 1.6
+
+set ANT_HOME=c:\apache-ant-1.6.5
+
+set JAVA_HOME=C:\j2sdk1.4.2_13
+set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
+call java -version
+
+call ant -Djdbc.level=3 ydoc.3
+
+set JAVA_HOME=c:\jdk1.6.0
+set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
+call java -version
+
+call ant -Djdbc.level=4 ydoc.4
