9da5241e39f82190a9342bc84766bef8e634d7ac
arthur_blake <arthur_blake@localhost>
2008-03-12 21:46:14 +0000

    remove beta designation from version number and fix scripts

--- scripts/profsql.cmd
+++ scripts/profsql.cmd
@@ -25,4 +25,4 @@ rem *  profiling output report.                                                 
 rem *                                                                                               *
 rem *************************************************************************************************
 
-java -Xmx1024m -classpath ..\build\log4jdbc-1.1beta1.jar net.sf.log4jdbc.PostLogProfilerProcessor %*
+java -Xmx1024m -classpath ..\build\log4jdbc3-1.1.jar net.sf.log4jdbc.PostLogProfilerProcessor %*
