eac61725f635fc5d249ff6c4640c470014a5e1a7
arthur.blake <arthur.blake@localhost>
2009-08-08 19:02:49 +0000

    bump version number for 1.2 alpha 3 release

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -4,6 +4,11 @@ setlocal
 REM invoke the 1.4 and the 1.6 jvm each in turn 
 REM for JDBC 3 and JDBC 4 versions respectively
 
+REM BTW, It's not actually necessary to use JDK 1.4
+REM if you don't happen to have it laying around,
+REM as the ant script properly sets the source
+REM and target java versions anyway.
+
 set ANT_HOME=c:\apache-ant-1.6.5
 
 set JAVA_HOME=C:\jdk\1.4.2_18
