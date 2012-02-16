7f7d2f0f3b186f8b5372671c651933f2069ce60a
arthur.blake <arthur.blake@localhost>
2012-02-16 22:31:04 +0000

    update build scripts to use latest 1.6 jdk and ant releases

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -9,7 +9,7 @@ REM if you don't happen to have it laying around,
 REM as the ant script properly sets the source
 REM and target java versions anyway.
 
-set ANT_HOME=c:\apache-ant-1.8.1
+set ANT_HOME=c:\apache-ant-1.8.2
 
 set JAVA_HOME=C:\jdk\1.4.2_19
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
@@ -17,7 +17,7 @@ set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
 call java -version
 call ant -Djdbc.level=3 -Djvm.ver=1.4 all
 
-set JAVA_HOME=C:\jdk\1.6.0_21
+set JAVA_HOME=C:\jdk\1.6.0_30
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
 
 call java -version
--- scripts/javadoc.cmd
+++ scripts/javadoc.cmd
@@ -4,7 +4,7 @@ setlocal
 REM invoke the 1.4 and the 1.6 jvm each in turn 
 REM for JDBC 3 and JDBC 4 versions respectively
 
-set ANT_HOME=c:\apache-ant-1.8.1
+set ANT_HOME=c:\apache-ant-1.8.2
 set ANT_OPTS=-Xmx512m
 
 set JAVA_HOME=C:\jdk\1.4.2_19
@@ -13,7 +13,7 @@ set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
 call java -version
 call ant -Djdbc.level=3 -Djvm.ver=1.4 ydoc.3
 
-set JAVA_HOME=c:\jdk\1.6.0_18
+set JAVA_HOME=c:\jdk\1.6.0_30
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
 
 call java -version
