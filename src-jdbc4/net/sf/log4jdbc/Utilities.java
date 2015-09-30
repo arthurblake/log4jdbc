77507faf9c3527c52932d743cd2fc5ef4ef410ec
Arthur Blake <arthur.blake@gmail.com>
2015-09-30 12:30:16 -0400

    add new option, "log4jdbc.trim.sql.lines"
    Unforunately, got mixed up with a partial code reformat.
    Too lazy to go back and do that part as a separate commit

--- src-jdbc4/net/sf/log4jdbc/Utilities.java
+++ src-jdbc4/net/sf/log4jdbc/Utilities.java
@@ -18,26 +18,45 @@ package net.sf.log4jdbc;
 /**
  * Static utility methods for use throughout the project.
  */
-public class Utilities {
-  /**
-   * Right justify a field within a certain number of spaces.
-   * @param fieldSize field size to right justify field within.
-   * @param field contents to right justify within field.
-   * @return the field, right justified within the requested size.
-   */
-  public static String rightJustify(int fieldSize, String field)
-  {
-    if (field==null)
-    {
-      field="";
-    }
-    StringBuffer output = new StringBuffer();
-    for (int i=0, j = fieldSize-field.length(); i < j; i++)
-    {
-      output.append(' ');
-    }
-    output.append(field);
-    return output.toString();
-  }
+public class Utilities
+{
+	/**
+	 * Right justify a field within a certain number of spaces.
+	 * @param fieldSize field size to right justify field within.
+	 * @param field contents to right justify within field.
+	 * @return the field, right justified within the requested size.
+	 */
+	public static String rightJustify(int fieldSize, String field)
+	{
+		if (field == null)
+		{
+			field = "";
+		}
+		StringBuffer output = new StringBuffer();
+		for (int i = 0, j = fieldSize - field.length(); i < j; i++)
+		{
+			output.append(' ');
+		}
+		output.append(field);
+		return output.toString();
+	}
 
+	/**
+	 * Trim whitespace off the right of a string.
+	 * @param s input String to trim.
+	 * @return output trimmed string.
+	 */
+	public static String rtrim(String s)
+	{
+		if (s == null)
+		{
+			return null;
+		}
+		int i = s.length() - 1;
+		while (i >= 0 && Character.isWhitespace(s.charAt(i)))
+		{
+			i--;
+		}
+		return s.substring(0, i + 1);
+	}
 }
